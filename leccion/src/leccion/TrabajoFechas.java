7997e260052848f0c4d2a63e2e12031ceb82853b
Usuario <Usuario@DESKTOP-BHC0AI5>
2019-07-16 19:30:06 -0500

    Trabajo en clase OO

--- leccion/src/leccion/TrabajoFechas.java
+++ leccion/src/leccion/TrabajoFechas.java
@@ -1,7 +1,23 @@
 
 package leccion;
 
+import java.util.Date;
+
 
 public class TrabajoFechas {
+    public static void main(String[] args) {
+      Date fecha=new Date(118,9,16); 
+        System.out.println(fecha);
+        Date fechas=new Date();
+        System.out.println(fechas);
+        System.out.println("Año: "+(fechas.getYear()+1900));
+        System.out.println("mes: "+fechas.getMonth());
+        System.out.println("dia: "+fechas.getDay());
+        Date fechass=new Date(101,6,18);
+        Date fechaActual=new Date();
+        System.out.println(fechass.after(fechaActual));
+        System.out.println(fechass.before(fechaActual));
+    }
+    
     
 }
